92ee4f7694018de2bf39957395c7e29db56c49b8
Jessica Murillo <jmurillo@alglaw.com>
2018-10-12 13:33:03 -0700

    vegasroots

---
+++ inc/header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/gallery-grid.css">
+    <link href="https://fonts.googleapis.com/css?family=Niramit|Open+Sans" rel="stylesheet">
+    <title></title>
+
+  </head>
+
+  <body>
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">Vegas Roots</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Blog</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Programs</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Events</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Get Involved</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
---
+++ index.php
@@ -0,0 +1,71 @@
+<?php include 'inc/header.php' ?>
+
+<!-- Page Content -->
+<main>
+  <div class="fluid-container">
+    <div class="jumbotron hero">
+      <div class="container" id="hero-content">
+        <h1 class="display-4">Vegas Roots</h1>
+        <p class="lead">A COMMUNITY GARDEN BASED IN DOWNTOWN LAS VEGAS</p>
+        <hr class="my-4">
+        <p>Nourishing the community with freshly grown foods!</p>
+        <p class="lead">
+          <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+        </p>
+      </div>
+    </div>
+  </div>
+
+  <div class="container gallery-container">
+
+      <div class="tz-gallery">
+
+          <div class="row">
+              <div class="col-sm-6 col-md-4">
+                  <a class="lightbox" href="http://vegasroots.org/wp-content/uploads/2017/10/staff-0025-vegasroots-0517.jpg">
+                      <img src="http://vegasroots.org/wp-content/uploads/2017/10/staff-0025-vegasroots-0517.jpg" alt="Park">
+                  </a>
+              </div>
+              <div class="col-sm-6 col-md-4">
+                  <a class="lightbox" href="http://vegasroots.org/wp-content/uploads/2017/10/Screen-Shot-2017-10-29-at-10.20.27-AM.png">
+                      <img src="http://vegasroots.org/wp-content/uploads/2017/10/Screen-Shot-2017-10-29-at-10.20.27-AM.png" alt="Bridge">
+                  </a>
+              </div>
+              <div class="col-sm-12 col-md-4">
+                  <a class="lightbox" href="http://vegasroots.org/wp-content/uploads/2015/08/marketplace-14-vegasroots-0817.jpg">
+                      <img src="http://vegasroots.org/wp-content/uploads/2015/08/marketplace-14-vegasroots-0817.jpg" alt="Tunnel">
+                  </a>
+              </div>
+              <div class="col-sm-6 col-md-4">
+                  <a class="lightbox" href="http://vegasroots.org/wp-content/uploads/2015/08/marketplace-13-vegasroots-0717-1.jpg">
+                      <img src="http://vegasroots.org/wp-content/uploads/2015/08/marketplace-13-vegasroots-0717-1.jpg" alt="Coast">
+                  </a>
+              </div>
+              <div class="col-sm-6 col-md-4">
+                  <a class="lightbox" href="http://vegasroots.org/wp-content/uploads/2017/10/vegasroots-women-2017.jpg">
+                      <img src="http://vegasroots.org/wp-content/uploads/2017/10/vegasroots-women-2017.jpg" alt="Rails">
+                  </a>
+              </div>
+              <div class="col-sm-6 col-md-4">
+                  <a class="lightbox" href="http://vegasroots.org/wp-content/uploads/2015/08/forrest-griffin-vegasroots2017-2.jpg">
+                      <img src="http://vegasroots.org/wp-content/uploads/2015/08/forrest-griffin-vegasroots2017-2.jpg" alt="Traffic">
+                  </a>
+              </div>
+              <div class="col-sm-6 col-md-4">
+                  <a class="lightbox" href="http://vegasroots.org/wp-content/uploads/2017/05/marketplace-21-vegasroots-0517.jpg">
+                      <img src="http://vegasroots.org/wp-content/uploads/2017/05/marketplace-21-vegasroots-0517.jpg" alt="Rocks">
+                  </a>
+              </div>
+          </div>
+
+      </div>
+
+  </div>
+
+</main>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.js"></script>
+<script>
+    baguetteBox.run('.tz-gallery');
+</script>
+
+<?php include 'inc/footer.php' ?>
